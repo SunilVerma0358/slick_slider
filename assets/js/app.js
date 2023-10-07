333b113914ba6121d74bd2dc7c3eafa0a1a65dde
SunilVerma0358 <sunilverma93060@gmail.com>
2023-10-07 13:59:23 +0530

    slick-slider

---
+++ assets/js/app.js
@@ -0,0 +1,54 @@
+// preloader
+const preloader = document.getElementById("preloader")
+setTimeout(() => {
+        preloader.classList.add("d-none")
+        preloader.classList.add("pointer-event-none")
+        document.body.classList.remove("overflow-hidden")
+    }, 3000)
+    // slider
+$('.slider-for').slick({
+    arrow: false,
+    centerMode: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-nav',
+    responsive: [{
+            breakpoint: 991.5,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 767.5,
+            settings: {
+                centerMode: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 575.5,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.slider-nav').slick({
+    arrow: false,
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    focusOnSelect: true
+});
+
+$('a[data-slide]').click(function(e) {
+    e.preventDefault();
+    var slideno = $(this).data('slide');
+    $('.slider-nav').slick('slickGoTo', slideno - 1);
+});
